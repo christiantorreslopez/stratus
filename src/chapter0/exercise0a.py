6da913eb6a82e355b55965dd764cf49e079deec6
christiantorreslopez <torreslopez188@gmail.com>
2025-10-31 16:38:19 +0000

    I just did the chapter0 exercises.

--- src/chapter0/exercise0a.py
+++ src/chapter0/exercise0a.py
@@ -6,7 +6,7 @@ This is a python file called exercise0.py. The .py extension is what tells the c
 The `print` function is used to display text or other output to the screen. Remember, programming languages have their own syntax and rules. In Python, the syntax for the `print` function looks like this below:
 """
 
-print("Hello")
+print(Hello World)
 
 """
 In your terminal, type: python src/chapter0/exercise0a.py
